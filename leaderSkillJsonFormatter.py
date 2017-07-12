e63e6859eb58e8b3bc5fb3dedeb35eea1ce17ddc
Edman Feng <edmanfeng@hotmail.com>
2017-07-12 03:37:05 -0700

    started to work on generic combos

--- leaderSkillJsonFormatter.py
+++ leaderSkillJsonFormatter.py
@@ -1,6 +1,106 @@
 import json
 import re
 
+attributePattern = r'''
+    (fire|water|wood|light|dark)
+
+'''
+attributeRe = re.compile(attributePattern, re.IGNORECASE|re.VERBOSE)
+
+typePattern = r'''
+    (
+    god|balanced|attacker|physical
+    |devil|healer|dragon|machine
+    |evo material|awaken material
+    |enhance material|redeemable material
+    )
+'''
+typeRe = re.compile(typePattern, re.IGNORECASE|re.VERBOSE)
+
+
+#basic skills are just are type and attribute multipliers
+basicRePattern = r'''
+    ((((
+    ''' + attributePattern + "|" + typePattern + ''')   #list of attributes and types
+    
+    \W+ (attribute\W+|type\W+)?)+   #repeat for all attributes and types
+
+    cards\W+
+    
+    ((hp|atk|rcv|all[ ]stats)[ ]x\d([.]\d)?(,[ ])?)+)[.]?$) #followed by multipliers
+
+'''
+basicRe = re.compile(basicRePattern, re.IGNORECASE|re.VERBOSE)
+
+basicMultiPattern = r'''
+    cards[ ]
+    (?:
+    (?:all[ ]stats[ ]x(\d(?:[.]\d)?))|
+    (?:HP[ ]x(\d(?:[.]\d)?))?       #captures the HP multiplier
+    (?:,[ ])?
+    (?:atk[ ]x(\d(?:\.\d)?))?       #captures atk multiplier
+    (?:,[ ])?
+    (?:rcv[ ]x(\d(?:\.\d)?))?        #captures rcv multiplier
+    [.]?
+    )
+'''
+basicMultiRe = re.compile(basicMultiPattern, re.IGNORECASE|re.VERBOSE)
+
+genericComboBasePattern = r'''
+    atk[ ]x(\d(?:\.\d)?)        #capture atk multiplier
+    [ ]at[ ](\d)[ ]combos       #capture base combo
+'''
+genericComboScalePattern = r'''
+    atk[ ]x(\d)                             #capture scaling mutliplier
+    [ ]for[ ]each[ ]additional[ ]combo,
+    [ ]up[ ]to[ ]atk[ ]x(\d+)[ ]at[ ]       #capture multiplier limit
+    (\d+)[ ]combos                          #capture combo limit
+'''
+
+def getBasicSkill(regexMatches):
+    result = ""
+    basicStr = regexMatches.group().strip(" ")
+    attributeM = attributeRe.findall(basicStr)
+    typeM = typeRe.findall(basicStr)
+
+    result += "{\"skilltype\":\"basic\","
+    result += "\"attribute\":["
+    if attributeM:
+        for attribute in attributeM:
+            result += "\"" + attribute + "\","
+    result = result.strip(",") + "],"
+
+    result += "\"type\":["
+    if typeM:
+        for type in typeM:
+            result += "\"" + type + "\","
+    result = result.strip(",") + "],"
+
+    multiplierM = basicMultiRe.search(basicStr)
+    hp = 1
+    atk = 1
+    rcv = 1
+    if multiplierM:
+        if multiplierM[1]:
+            hp = multiplierM[1]
+            atk = multiplierM[1]
+            rcv = multiplierM[1]
+        else:
+            hp = multiplierM[2] if multiplierM[2] else 1
+            atk = multiplierM[3] if multiplierM[3] else 1
+            rcv = multiplierM[4] if multiplierM[4] else 1
+    result += "\"effect\":{"
+    result += "\"hp\":" + str(hp) + ","
+    result += "\"atk\":" + str(atk) + ","
+    result += "\"rcv\":" + str(rcv)
+
+    result += "},"
+    result += "\"description\":\"" + basicStr + "\""
+    result += "},"
+    return result
+    
+def getGenericComboSkill(baseMatches, scaleMatches):
+
 def main():
     file = open("sampleLeaderSkills.json")
     leaderJson = json.load(file)
@@ -8,35 +108,7 @@ def main():
     if len(leaderJson) is 0:
         return
     
-    attributeRePattern = r'''
-        (fire|water|wood|light|dark)
     
-    '''
-    attributeRe = re.compile(attributeRePattern, re.IGNORECASE|re.VERBOSE)
-    
-    typeRePattern = r'''
-        (
-        god|balanced|attacker|physical
-        |devil|healer|dragon|machine
-        |evo material|awaken material
-        |enhance material|redeemable material
-        )
-    '''
-    typeRe = re.compile(typeRePattern, re.IGNORECASE|re.VERBOSE)
-    
-    #basic skills are just are type and attribute multipliers
-    basicRePattern = r'''
-        ((((
-        ''' + attributeRePattern + "|" + typeRePattern + ''')   #list of attributes and types
-        
-        \W+ (attribute\W+|type\W+)?)+   #repeat for all attributes and types
-   
-        cards\W+
-        
-        ((hp|atk|rcv|all[ ]stats)[ ]x\d([.]\d)?(,[ ])?)+)[.]?$) #followed by multipliers
-
-    '''
-    basicRe = re.compile(basicRePattern, re.IGNORECASE|re.VERBOSE)
     
     result = "["
     for skillJson in leaderJson:
@@ -51,30 +123,17 @@ def main():
         i = 0
         while i < len(leaderSkillParts):
             part = leaderSkillParts[i]
-            m = basicRe.search(part)
-            print(part)
-            if m:
-                print("yes")
-                basicStr = m.group().strip(" ")
-                attributeM = attributeRe.findall(basicStr)
-                typeM = typeRe.findall(basicStr)
-                
-                result += "{\"skilltype\":\"basic\","
-                result += "\"attribute\":["
-                if attributeM:
-                    for attribute in attributeM:
-                        result += "\"" + attribute + "\","
-                result = result.strip(",") + "],"
-                result += "\"type\":["
-                if typeM:
-                    for type in typeM:
-                        result += "\"" + type + "\","
-                result = result.strip(",") + "],"
-
-                result += "\"effect\":{},"
-                result += "\"description\":\"" + basicStr + "\""
-                result += "},"
             i += 1
+            basicM = basicRe.search(part)
+            if basicM:
+                result += getBasicSkill(basicM)
+                break
+            
+            genericComboBaseRe = re.compile(genericComboBasePattern, re.IGNORECASE|re.VERBOSE)
+            genericComboBaseM = genericComboBaseRe.search(part)
+            if genericComboBaseM:
+                scalePart = leaderSkillParts[i + 1]     #i already incremented
+                result += getGenericComboSkill(genericComboBaseM)
         result = result.strip(",") # fencepost problem
         result += "]},"
     result = result.strip(",")
@@ -85,7 +144,7 @@ def main():
     outFile.close()
     file.close()
     
-    
+
 
 if __name__ == "__main__":
     main()
